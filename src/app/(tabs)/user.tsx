6aea2f9711a100698693a06d6354fac6da80e474
malfaro21 <malfaro21@students.desu.edu>
2024-04-06 16:10:18 -0400

    added navigation to user screen

--- src/app/(tabs)/user.tsx
+++ src/app/(tabs)/user.tsx
@@ -1,21 +1,52 @@
-import { Text, View } from '@/src/components/Themed';
-import React from 'react';
-import { StyleSheet, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Pressable, Text, View } from 'react-native';
+import Modal from 'react-native-modal';
 import { FontAwesome } from '@expo/vector-icons';
-
+import { useNavigation } from '@react-navigation/native';
 
 export default function UserScreen() {
+  const [isAchievementsModalVisible, setIsAchievementsModalVisible] = useState(false);
+  const [isLeaderboardModalVisible, setIsLeaderboardModalVisible] = useState(false);
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
-      <Pressable style={styles.achievementsBox}> 
+      <Pressable
+        style={styles.achievementsBox}
+        onPress={() => setIsAchievementsModalVisible(true)}>
         <Text style={styles.achievementsTitle}>Achievements</Text>
-        <FontAwesome name="pencil" size={24} color="black" style={styles.arrowIcon} /> 
-      </Pressable> 
+        <FontAwesome name="pencil" size={24} color="black" style={styles.arrowIcon} />
+      </Pressable>
 
-      <Pressable style={styles.friendsBox}> 
+      <Pressable
+        style={styles.friendsBox}
+        onPress={() => setIsLeaderboardModalVisible(true)}>
         <Text style={styles.friendsTitle}>Friends</Text>
         <FontAwesome name="pencil" size={24} color="black" style={styles.arrowIcon} />
-      </Pressable> 
+      </Pressable>
+
+      <Modal isVisible={isAchievementsModalVisible} style={styles.modalContent}>
+        <Pressable onPress={() => {
+          navigation.navigate('achievements');
+          setIsAchievementsModalVisible(false);
+        }}>
+          <Text style={styles.modalText}>Go to Achievements</Text>
+        </Pressable>
+        <Pressable onPress={() => setIsAchievementsModalVisible(false)}>
+          <FontAwesome name="close" size={24} color="black" style={{ alignSelf: 'flex-end' }} />
+        </Pressable>
+      </Modal>
+      <Modal isVisible={isLeaderboardModalVisible} style={styles.modalContent}>
+        <Pressable onPress={() => {
+          navigation.navigate('leaderboard');
+          setIsLeaderboardModalVisible(false);
+        }}>
+          <Text style={styles.modalText}>Go to Leaderboard</Text>
+        </Pressable>
+        <Pressable onPress={() => setIsLeaderboardModalVisible(false)}>
+          <FontAwesome name="close" size={24} color="black" style={{ alignSelf: 'flex-end' }} />
+        </Pressable>
+      </Modal>
     </View>
   );
 }
@@ -27,13 +58,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   achievementsBox: {
-    backgroundColor: '#ccc', 
-    width: 350, 
-    height: 150, 
+    backgroundColor: '#ccc',
+    width: 350,
+    height: 150,
     position: 'absolute',
-    top: 200,  
-    borderRadius: 10, 
-    shadowColor: '#000', 
+    top: 200,
+    borderRadius: 10,
+    shadowColor: '#000',
     shadowOffset: { width: 3, height: 3 },
     shadowOpacity: 0.5,
     shadowRadius: 5,
@@ -42,16 +73,16 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: 10, 
+    marginTop: 10,
   },
   friendsBox: {
-    backgroundColor: '#ccc', 
-    width: 350, 
-    height: 150, 
+    backgroundColor: '#ccc',
+    width: 350,
+    height: 150,
     position: 'absolute',
-    top: 370,  
-    borderRadius: 10, 
-    shadowColor: '#000', 
+    top: 370,
+    borderRadius: 10,
+    shadowColor: '#000',
     shadowOffset: { width: 3, height: 3 },
     shadowOpacity: 0.5,
     shadowRadius: 5,
@@ -60,11 +91,23 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: 10, 
+    marginTop: 10,
   },
   arrowIcon: {
     position: 'absolute',
     bottom: 10,
-    right: 10, 
+    right: 10,
   },
+  modalContent: {
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    width: '90%',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+  },
+  modalText: {
+    fontSize: 18,
+  }
 });
